d5e5cc2acc27561255f398579891477e24fac14f
nodescout <107863038+nodescout@users.noreply.github.com>
2023-11-06 17:46:14 -0400

    Updated `_DBFilePath` value to work in unix and windows os configs.

--- SQLiteDatabase.cs
+++ SQLiteDatabase.cs
@@ -2,30 +2,33 @@ namespace SQLiteWrap
 {
     using System;
     using System.Data.SQLite;
+    using System.IO;
 
     /// <summary>
-    /// Wraps around a Sqlite database to provide core functionality,
-    /// such as, create, open, close database etc...
+    ///     Wraps around a Sqlite database to provide core functionality,
+    ///     such as, create, open, close database etc...
     /// </summary>
     public class SQLiteDatabase
     {
         #region fields
-        private string _DBFilePath = @".\";
+        private string _DBFilePath = @"./";
         private string _DBFileName = "database.sqlite";
 
         private SQLiteConnection _Connection = null;
 
         private bool _EnforceForeignKeys = true;
         private bool _MutliThreadAccess = false;
+
         #endregion fields
 
         #region constructors
+
         /// <summary>
-        /// Class Constructor
+        ///     Class Constructor
         /// </summary>
         public SQLiteDatabase(string dbFileName = null,
-                  bool enforceForeignKeys = true,
-                  bool mutliThreadAccess = false)
+            bool enforceForeignKeys = true,
+            bool mutliThreadAccess = false)
         {
             if (string.IsNullOrEmpty(dbFileName) == false)
                 _DBFileName = dbFileName;
@@ -33,108 +36,91 @@ namespace SQLiteWrap
             Status = "Not connected - no connection initialized.";
 
             ExtendendStatus = string.Empty;
-            this.Exception = null;
+            Exception = null;
 
             _EnforceForeignKeys = enforceForeignKeys;
             _MutliThreadAccess = mutliThreadAccess;
         }
+
         #endregion constructors
 
         #region properties
+
         /// <summary>
-        /// Gets the name and path of the SQLite file in which the tree
-        /// model data is to be stored.
+        ///     Gets the name and path of the SQLite file in which the tree
+        ///     model data is to be stored.
         /// </summary>
         public string DBFileNamePath
         {
-            get
-            {
-                return System.IO.Path.Combine(_DBFilePath, _DBFileName);
-            }
+            get { return Path.Combine(_DBFilePath, _DBFileName); }
         }
 
         /// <summary>
-        /// Gets whether Foreign Keys are:
-        /// - Enforced in the SQLite database file
-        ///   (wrong values will result in exception on update or insert) or
-        ///   
-        /// - NOT enforced
-        ///   (wrong values are ignored by SQLite).
+        ///     Gets whether Foreign Keys are:
+        ///     - Enforced in the SQLite database file
+        ///     (wrong values will result in exception on update or insert) or
+        ///     - NOT enforced
+        ///     (wrong values are ignored by SQLite).
         /// </summary>
-        public bool EnforceForeignKeys { get { return _EnforceForeignKeys; } }
+        public bool EnforceForeignKeys
+        {
+            get { return _EnforceForeignKeys; }
+        }
 
         /// <summary>
-        /// Gets whether the SQLite database file can be accessed from within multiple
-        /// threads and/or connections or not.
+        ///     Gets whether the SQLite database file can be accessed from within multiple
+        ///     threads and/or connections or not.
         /// </summary>
-        public bool MutliThreadAccess { get { return _MutliThreadAccess; } }
+        public bool MutliThreadAccess
+        {
+            get { return _MutliThreadAccess; }
+        }
 
         /// <summary>
-        /// Gets the Name of the SQLite file in which the tree
-        /// model data is to be stored.
+        ///     Gets the Name of the SQLite file in which the tree
+        ///     model data is to be stored.
         /// </summary>
         public string DBFileName
         {
-            get
-            {
-                return _DBFileName;
-            }
+            get { return _DBFileName; }
         }
 
         /// <summary>
-        /// Gets the Path of the SQLite file in which the tree
-        /// model data is to be stored.
+        ///     Gets the Path of the SQLite file in which the tree
+        ///     model data is to be stored.
         /// </summary>
         public string DBFilePath
         {
-            get
-            {
-                return _DBFilePath;
-            }
+            get { return _DBFilePath; }
         }
 
         /// <summary>
-        /// Gets extended information on exceptions that might have
-        /// occurred to reach the current status.
+        ///     Gets extended information on exceptions that might have
+        ///     occurred to reach the current status.
         /// </summary>
-        public Exception Exception
-        {
-            get;
-            set;
-        }
+        public Exception Exception { get; set; }
 
         /// <summary>
-        /// Gets a textual description of the current SQLite database status.
+        ///     Gets a textual description of the current SQLite database status.
         /// </summary>
-        public string Status
-        {
-            get;
-            private set;
-        }
+        public string Status { get; private set; }
 
         /// <summary>
-        /// Gets/sets additional error/state information (if any).
+        ///     Gets/sets additional error/state information (if any).
         /// </summary>
-        public string ExtendendStatus
-        {
-            get;
-            set;
-        }
+        public string ExtendendStatus { get; set; }
 
         /// <summary>
-        /// Gets a connection object that can be uses
-        /// to interact with an existing and open SQLite database.
+        ///     Gets a connection object that can be uses
+        ///     to interact with an existing and open SQLite database.
         /// </summary>
         public SQLiteConnection Connection
         {
-            get
-            {
-                return _Connection;
-            }
+            get { return _Connection; }
         }
 
         /// <summary>
-        /// Gets whether the database connection is currently established (open), or not (false).
+        ///     Gets whether the database connection is currently established (open), or not (false).
         /// </summary>
         public bool ConnectionState
         {
@@ -153,20 +139,21 @@ namespace SQLiteWrap
                 catch (Exception exp)
                 {
                     Status = exp.Message;
-                    this.Exception = exp;
+                    Exception = exp;
 
                     throw new Exception(exp.Message);
                 }
             }
         }
+
         #endregion properties
 
         #region methods
+
         /// <summary>
-        /// Opens a connection to a SQLite database if there is none already open.
-        /// 
-        /// The previously existing database is deleted if <paramref name="overwriteFile"/>
-        /// is true.
+        ///     Opens a connection to a SQLite database if there is none already open.
+        ///     The previously existing database is deleted if <paramref name="overwriteFile" />
+        ///     is true.
         /// </summary>
         /// <param name="overwriteFile"></param>
         public void OpenConnection(bool overwriteFile = false)
@@ -186,14 +173,17 @@ namespace SQLiteWrap
             catch (Exception exp)
             {
                 Status = exp.Message;
-                this.Exception = exp;
+                Exception = exp;
 
+                Console.WriteLine(exp.Message);
+                Console.WriteLine(exp.StackTrace);
+                Console.WriteLine(exp.Source);
                 throw new Exception(exp.Message);
             }
         }
 
         /// <summary>
-        /// Closes any open connections to the SQLite database.
+        ///     Closes any open connections to the SQLite database.
         /// </summary>
         public void CloseConnection()
         {
@@ -207,16 +197,17 @@ namespace SQLiteWrap
             catch (Exception exp)
             {
                 Status = exp.Message;
-                this.Exception = exp;
+                Exception = exp;
 
                 throw new Exception(exp.Message);
             }
         }
 
         #region Pragma UserVersion
+
         /// <summary>
-        /// Gets the current user version of the currently
-        /// opened database (or throws an exception if database was unavailable).
+        ///     Gets the current user version of the currently
+        ///     opened database (or throws an exception if database was unavailable).
         /// </summary>
         /// <returns></returns>
         public long UserVersion()
@@ -232,15 +223,15 @@ namespace SQLiteWrap
             catch (Exception exp)
             {
                 Status = exp.Message;
-                this.Exception = exp;
+                Exception = exp;
 
                 throw new Exception(exp.Message);
             }
         }
 
         /// <summary>
-        /// Method increases the current user version of the currently
-        /// opened database (or throw an exception if database was unavailable).
+        ///     Method increases the current user version of the currently
+        ///     opened database (or throw an exception if database was unavailable).
         /// </summary>
         /// <returns></returns>
         public long UserVersionIncrease()
@@ -260,17 +251,19 @@ namespace SQLiteWrap
             catch (Exception exp)
             {
                 Status = exp.Message;
-                this.Exception = exp;
+                Exception = exp;
 
                 throw new Exception(exp.Message);
             }
         }
+
         #endregion Pragma UserVersion
 
         #region Pragma JournalMode
+
         /// <summary>
-        /// Gets the current journal mode of the currently
-        /// opened database (or throws an exception if database was unavailable).
+        ///     Gets the current journal mode of the currently
+        ///     opened database (or throws an exception if database was unavailable).
         /// </summary>
         /// <returns></returns>
         public string JournalMode()
@@ -282,21 +275,21 @@ namespace SQLiteWrap
                     cmd.CommandText = "pragma journal_mode;";
                     var result = cmd.ExecuteScalar();
 
-                    return result as string;           // default is "delete"
+                    return result as string; // default is "delete"
                 }
             }
             catch (Exception exp)
             {
                 Status = exp.Message;
-                this.Exception = exp;
+                Exception = exp;
 
                 throw new Exception(exp.Message);
             }
         }
 
         /// <summary>
-        /// Method sets the journal mode of the currently
-        /// opened database (or throws an exception if database was unavailable).
+        ///     Method sets the journal mode of the currently
+        ///     opened database (or throws an exception if database was unavailable).
         /// </summary>
         /// <returns></returns>
         public void JournalMode(JournalMode journalMode)
@@ -315,11 +308,12 @@ namespace SQLiteWrap
             catch (Exception exp)
             {
                 Status = exp.Message;
-                this.Exception = exp;
+                Exception = exp;
 
                 throw new Exception(exp.Message);
             }
         }
+
         #endregion Pragma JournalMode
 
         private void ConstructConnection(bool overWriteFile = false)
@@ -333,7 +327,7 @@ namespace SQLiteWrap
 
             _Connection = new SQLiteConnection(connectString.ToString());
 
-            if (System.IO.File.Exists(dbFileNamePath) == false)
+            if (File.Exists(dbFileNamePath) == false)
             {
                 // Overwrites a file if it is already there
                 SQLiteConnection.CreateFile(dbFileNamePath);
@@ -355,14 +349,15 @@ namespace SQLiteWrap
         }
 
         /// <summary>
-        /// Determines the journal model of the SQLite database - this is
-        /// required to at conneciton/database open time.
+        ///     Determines the journal model of the SQLite database - this is
+        ///     required to at conneciton/database open time.
         /// </summary>
         /// <returns></returns>
         private SQLiteJournalModeEnum GetJournalMode()
         {
-            return (this.MutliThreadAccess ? SQLiteJournalModeEnum.Wal : SQLiteJournalModeEnum.Default);
+            return MutliThreadAccess ? SQLiteJournalModeEnum.Wal : SQLiteJournalModeEnum.Default;
         }
+
         #endregion methods
     }
 }
